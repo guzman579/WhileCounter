ab16712458c43d94bc4ba2fb540f10dbfa5cf3be
Irene <35508977+guzman579@users.noreply.github.com>
2019-02-06 13:25:31 -0800

    Create WhileCounter.java

---
+++ WhileCounter.java
@@ -0,0 +1,17 @@
+//WhileCounter.java
+// Counter-controlled repetition with the while repetition statement.
+public class WhileCouter
+{
+   public static void main(String[] args) 
+   {      
+      int counter = 1; // declare and initialize control variable
+
+      while (counter <= 10) // loop-continuation condition
+      {
+         System.out.printf("%d  ", counter);
+         ++counter; // increment control variable 
+      } 
+
+      System.out.println(); 
+   } 
+} // end class 
